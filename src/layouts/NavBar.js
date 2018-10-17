994f431c08e1dd271d0f620d06d0fbc5e147163d
dblasher <dblasher90@gmail.com>
2018-10-16 17:55:52 -0700

    added Tic Tac Toe to Projects Tab

--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -2,7 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { AppBar, Toolbar, Typography, Tabs, Tab } from '@material-ui/core';
 import About from './About';
+import TicTac from '../projects/TicTac';
 
+//red #ef104c
+//green #10ed40
 
 function TabContainer(props) {
     return (
@@ -26,10 +29,10 @@ class NavBar extends React.Component {
     };
     render() {
         const { value } = this.state;
-
+        //const { classes } = this.props;
         return (
-            <div style={{ flexGrow: 1 }}>
-                <AppBar position="static">
+            <div style={{ flexGrow: 2 }}>
+                <AppBar position="static" color = "secondary">
                     <Toolbar>
                         <Typography variant="title" color="inherit">
                             Daniel Blasher
@@ -37,13 +40,13 @@ class NavBar extends React.Component {
                         <Tabs value={value} onChange={this.handleChange}>
                             <Tab label="About" />
                             <Tab label="Projects" />
-                            <Tab label="Contact" href="#basic-tabs" />
+                            <Tab label="Contact" />
                         </Tabs>
 
                     </Toolbar>
                 </AppBar>
                 {value === 0 && <About/>}
-                {value === 1 && <TabContainer>Projects</TabContainer>}
+                {value === 1 && <TabContainer>Tic Tac Toe!<TicTac/>Weather!</TabContainer>}
                 {value === 2 && <TabContainer>Contact</TabContainer>}
             </div>
         )
