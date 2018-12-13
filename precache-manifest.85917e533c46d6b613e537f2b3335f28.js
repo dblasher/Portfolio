44d9ee357b79566832e3e50f49dc5a7a9e4ed5cb
dblasher <dblasher90@gmail.com>
2018-12-13 09:37:14 -0800

    Updates

---
+++ precache-manifest.85917e533c46d6b613e537f2b3335f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0b9efac38736bee8df45d0341319450",
+    "url": "/portfolio/static/media/Daniel.c0b9efac.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b25f912ac3952bf691c0",
+    "url": "/portfolio/static/js/main.b25f912a.chunk.js"
+  },
+  {
+    "revision": "e055b8635dd2e130beb8",
+    "url": "/portfolio/static/js/1.e055b863.chunk.js"
+  },
+  {
+    "revision": "b25f912ac3952bf691c0",
+    "url": "/portfolio/static/css/main.497ef076.chunk.css"
+  },
+  {
+    "revision": "6da6ff7099cbbe6c08acc5422a0f07a2",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2a887344f942334f9c79053cb1791fc5.js"
+  "/portfolio/precache-manifest.85917e533c46d6b613e537f2b3335f28.js"
 );
 
 workbox.clientsClaim();
