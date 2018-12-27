e0375868e6e2e2d42a0f7eaa8225796c495d1307
dblasher <dblasher90@gmail.com>
2018-12-26 18:50:58 -0800

    refactored, navbar component is merged into App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { AppBar, Toolbar, Typography, Button,  Tabs, Tab } from '@material-ui/core';
 import { NavBar, Footer } from './layouts';
 import './App.css';
-import TopNav from './layouts/TopNav';
+import Intro from './layouts/Intro';
+import About from './layouts/About';
+import Projects from './projects';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 class App extends Component {
+  state = {
+    value: 0,
+};
 
+handleChange = (event, value) => {
+    this.setState({ value });
+};
   //---begin render---
   render() {
+
+    const { value } = this.state;
     return (
       <div className="App">
-        <TopNav/>
-        <NavBar />
+
+        <div style={{ flexGrow: 2 }}>
+          <AppBar position="static" color="secondary">
+            <Toolbar>
+              <Typography variant="title" color="textPrimary">
+                Daniel Blasher
+                </Typography>
+              <Tabs value={value} onChange={this.handleChange}>
+                <Tab label="About" />
+                <Tab label="Projects" />
+                <Tab label="Contact" />
+              </Tabs>
+            </Toolbar>
+          </AppBar>
+          {value === 0 && <TabContainer><Intro /><About /></TabContainer>}
+          {value === 1 && <TabContainer><Projects /></TabContainer>}
+          {value === 2 && <TabContainer>Contact</TabContainer>}
+        </div>
         <Footer />
       </div>
     );
--- src/layouts/TopNav.js
+++ src/layouts/TopNav.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import { NavLink, Route, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '../icons/MenuIcon';
 
 const styles = {
   root: {
@@ -27,12 +26,12 @@ function ButtonAppBar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-          </IconButton>
           <Typography variant="title" color="inherit" className={classes.flex}>
-            Title
+            Daniel Blasher
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Projects</Button>
+          <Button color="inherit">Contact</Button>
         </Toolbar>
       </AppBar>
     </div>
